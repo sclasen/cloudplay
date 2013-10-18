b1be1353da3e0d05404036c63add98987e540810
Scott Clasen <scott@heroku.com>
2013-10-18 10:14:56 -0700

    init stab at docker jenkins

---
+++ ci/userdata.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+apt-get update
+apt-get install linux-image-extra-`uname -r`
+sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+apt-get update
+apt-get install lxc-docker
